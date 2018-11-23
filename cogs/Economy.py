693f898c3f305054723671221732f8a016e94402
EnterNewName <40485108+EnterNewName@users.noreply.github.com>
2018-11-23 16:36:27 -0600

    doing a backend upgrade

--- cogs/Economy.py
+++ cogs/Economy.py
@@ -10,32 +10,41 @@ class PewDieCoin:
     def __init__(self, bot):
         self.bot = bot
 
+
+    """async def cooldown(self, user : discord.Member, seconds : int):
+        await self.bot.db.execute("INSERT INTO cooldowns(user_id, start_time, end_time) VALUES ($1, $2, $3)")"""
+
     @commands.command(aliases=['$', 'balance'])
     async def bank(self, ctx):
-        count = await self.bot.db.fetchval("SELECT user_money FROM pdp_economy WHERE user_id=$1", ctx.author.id)
+        count = await self.bot.db.fetchval("SELECT user_money FROM bank WHERE user_id=$1", ctx.author.id)
         if count is None:
             count = 0
         await ctx.send(f"You currently have `{count}` coins")
 
-    @commands.cooldown(1, 14400, commands.BucketType.user)
     @commands.command()
     async def timely(self, ctx):
-        count = await self.bot.db.fetchval("SELECT user_money FROM pdp_economy WHERE user_id=$1", ctx.author.id)
+        count = await self.bot.db.fetchval("SELECT user_money FROM bank WHERE user_id=$1", ctx.author.id)
         if count is None:
             count = 0
-        await self.bot.db.execute("INSERT INTO pdp_economy (user_id, user_money) VALUES ($1, $2 + 75) ON CONFLICT (user_id) DO UPDATE SET user_money= $2 + 75", ctx.author.id, count)
-        after_money = await self.bot.db.fetchval("SELECT user_money FROM pdp_economy WHERE user_id=$1", ctx.author.id)
+        await self.bot.db.execute("INSERT INTO bank (user_id, user_money) VALUES ($1, $2 + 75) ON CONFLICT (user_id) DO UPDATE SET user_money= $2 + 75", ctx.author.id, count)
+        after_money = await self.bot.db.fetchval("SELECT user_money FROM bank WHERE user_id=$1", ctx.author.id)
         await ctx.send(f"Added `75` coins to your coin pouch, your current amount is now `{after_money}` coins")
 
     @commands.command()
     async def coinflip(self, ctx, side: str, amount_of_coins):
         result = random.choice(['h', 't'])
         amt = amount_of_coins
-        count = await self.bot.db.fetchval("SELECT user_money FROM pdp_economy WHERE user_id=$1", ctx.author.id)
+        count = await self.bot.db.fetchval("SELECT user_money FROM bank WHERE user_id=$1", ctx.author.id)
         if amt == 'all':
             amt = count
         else:
             amt = int(amt)
+        if (side == 'head' or side == 'heads') and result == 'h':
+            side = 'h'
+        elif (side == 'tail' or side == 'tails') and result == 't':
+            side = 't'
+        else:
+            side = side
         if count is None:
             count = 0
         if amt > count:
@@ -43,14 +52,14 @@ class PewDieCoin:
         else:
             if result == side:
                 await ctx.send(embed=discord.Embed(description=f"Congrats fellow Pewd, you won {amt} coins", color=discord.Color.green()))
-                await self.bot.db.execute("UPDATE pdp_economy SET user_money= user_money + $1 WHERE user_id=$2", amt, ctx.author.id)
+                await self.bot.db.execute("UPDATE bank SET user_money= user_money + $1 WHERE user_id=$2", amt, ctx.author.id)
             else:
                 await ctx.send(embed=discord.Embed(color=discord.Color.red(), description=f"Lost to T-Series by {amt} coins"))
-                await self.bot.db.execute("UPDATE pdp_economy SET user_money= user_money - $1 WHERE user_id=$2", amt, ctx.author.id)
+                await self.bot.db.execute("UPDATE bank SET user_money= user_money - $1 WHERE user_id=$2", amt, ctx.author.id)
 
     @commands.command()
     async def give(self, ctx, amt, user: discord.User):
-        author_count = await self.bot.db.fetchval("SELECT user_money FROM pdp_economy WHERE user_id=$1", ctx.author.id)
+        author_count = await self.bot.db.fetchval("SELECT user_money FROM bank WHERE user_id=$1", ctx.author.id)
         if amt == 'all':
             amt = author_count
         else:
@@ -64,14 +73,14 @@ class PewDieCoin:
         if amt > author_count:
             return await ctx.send(f"You have insufficient funds to give to {user.name}")
         else:
-            current_money = await self.bot.db.fetchval("SELECT * FROM pdp_economy WHERE user_id=$1", user.id)
+            current_money = await self.bot.db.fetchval("SELECT * FROM bank WHERE user_id=$1", user.id)
             if current_money is None:
                 current_money = 0
-            await self.bot.db.execute("""UPDATE pdp_economy 
+            await self.bot.db.execute("""UPDATE bank 
                                          SET user_money = user_money - $1
                                          WHERE user_id = $2""", amt, ctx.author.id)
 
-            await self.bot.db.execute("""INSERT INTO pdp_economy (user_id, user_money)
+            await self.bot.db.execute("""INSERT INTO bank (user_id, user_money)
                                          VALUES ($1, $2) ON CONFLICT (user_id)
                                          DO UPDATE 
                                          SET user_money = $3 + $2
--- schema.sql
+++ schema.sql
@@ -1,9 +1,21 @@
 -- To hold the command count of the bot, BIGINT is required since it isnt gonna be 1 thru 9 or some shit
-CREATE TABLE IF NOT EXISTS pdp_cmds (
+CREATE TABLE IF NOT EXISTS commands_used (
     cmdcount BIGINT NOT NULL DEFAULT 0
 );
 -- Creates the Economy Table
-CREATE TABLE IF NOT EXISTS pdp_economy (
+CREATE TABLE IF NOT EXISTS bank (
     user_id BIGINT PRIMARY KEY NOT NULL,
     user_money BIGINT NOT NULL DEFAULT 0
 );
+
+CREATE TABLE IF NOT EXISTS shop (
+    role_id BIGINT UNIQUE NOT NULL,
+    guild_id BIGINT PRIMARY KEY NOT NULL,
+)
+
+CREATE TABLE IF NOT EXISTS cooldowns (
+    user_id BIGINT PRIMARY KEY NOT NULL,
+    start_time INT NOT NULL,
+    end_time INT NOT NULL
+    
+)
