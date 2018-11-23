4d1abf81c76d89684e5a5a27454d43d715b11339
EnterNewName <40485108+EnterNewName@users.noreply.github.com>
2018-11-23 17:17:21 -0600

    thank you testing guild

--- schema.sql
+++ schema.sql
@@ -1,8 +1,7 @@
--- To hold the command count of the bot, BIGINT is required since it isnt gonna be 1 thru 9 or some shit
 CREATE TABLE IF NOT EXISTS commands_used (
     cmdcount BIGINT NOT NULL DEFAULT 0
 );
--- Creates the Economy Table
+
 CREATE TABLE IF NOT EXISTS bank (
     user_id BIGINT PRIMARY KEY NOT NULL,
     user_money BIGINT NOT NULL DEFAULT 0
@@ -10,11 +9,11 @@ CREATE TABLE IF NOT EXISTS bank (
 
 CREATE TABLE IF NOT EXISTS shop (
     role_id BIGINT UNIQUE NOT NULL,
-    guild_id BIGINT PRIMARY KEY NOT NULL,
+    guild_id BIGINT PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cooldowns (
     user_id BIGINT PRIMARY KEY NOT NULL,
     start_time INT NOT NULL,
     end_time INT NOT NULL
-)
+);
