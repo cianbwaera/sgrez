91f6d38f9dcabe6c6b2a58c72ee0ee2f69887ec3
EnterNewName <40485108+EnterNewName@users.noreply.github.com>
2018-11-25 16:36:34 -0600

    EVEN MORE TESTING

--- cogs/Economy.py
+++ cogs/Economy.py
@@ -68,7 +68,9 @@ class PewDieCoin:
 
     @commands.command()
     async def give(self, ctx, user : discord.Member, amount):
-        author_coin = await self.bot.db.fetchval("SELECT user_money FROM bank WHERE user_id=$1", ctx.author.id)
+        author_coin = await self.bot.db.fetchval("SELECT user_money FROM bank WHERE user_id=$1;", ctx.author.id)
+        if author_coin is None:
+            author_coin = 0
         if amount is not int and amount == "all":
             amount = int(author_coin)
         else: 
@@ -80,9 +82,9 @@ class PewDieCoin:
         elif user.id == ctx.author.id:
             return await ctx.send(r"Why, why are you trying to give money to your self ¯\_(ツ)_/¯")
         else:
-            await self.bot.db.execute("UPDATE bank SET user_money=user_money-$1 WHERE user_id=$2", amount, ctx.author.id)
-            await self.bot.db.execute("INSERT INTO bank (user_id, user_money) VALUES ($1, $2) ON CONFLICT (user_id) DO UPDATE SET user_money = user_money +$2 ", user.id, amount)
-            author_coin = await self.bot.db.fetchval("SELECT user_money FROM bank WHERE user_id=$1", ctx.author.id)
+            await self.bot.db.execute("UPDATE bank SET user_money=user_money-$1 WHERE user_id=$2;", amount, ctx.author.id)
+            await self.bot.db.execute("INSERT INTO bank (user_id, user_money) VALUES ($1, $2) ON CONFLICT (bank.user_id) DO UPDATE SET user_money = user_money+$2;", user.id, amount)
+            author_coin = await self.bot.db.fetchval("SELECT user_money FROM bank WHERE user_id=$1;", ctx.author.id)
             await ctx.send(embed=discord.Embed(description=f"Gave {amount} coins to {user.mention}, your current amount is now `{author_coin}` coins"))
 
 
