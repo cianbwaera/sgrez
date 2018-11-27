eaf756fddcaf4ca64290a877aa24619bd07bc098
EnterNewName <40485108+EnterNewName@users.noreply.github.com>
2018-11-26 21:34:11 -0600

    lol

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS bank (
 
 CREATE TABLE IF NOT EXISTS shop (
     role_id BIGINT UNIQUE NOT NULL,
-    guild_id BIGINT PRIMARY KEY NOT NULL,
+    guild_id BIGINT NOT NULL,
     shop_num INT NOT NULL,
     amount BIGINT NOT NULL
 );
