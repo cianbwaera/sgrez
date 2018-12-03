5f9d4a79a9bf07722a81c3b293210f16eba9e67d
EnterNewName <40485108+EnterNewName@users.noreply.github.com>
2018-12-02 22:26:45 -0600

    test

---
+++ cogs/Music.py
@@ -0,0 +1,43 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+ffmpeg_options = {
+    'option' : "-vn"
+}
+
+class Music:
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.group()
+    async def disstrack(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("Invalid Arguments")
+
+    @disstrack.command()
+    async def play(self, ctx):
+        if not await ctx.author.voice.channel.connect():
+            return await ctx.send("You are currently not connected to a Voice Channel")
+        else:
+            await ctx.author.voice.channel.connect()
+            music = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("./db/song1.mp3"))
+            await ctx.voice_client.play(music, after=lambda e: print(e) if e else None)
+            await ctx.send(f"Now playing `Bitch Lasanga` in `{ctx.author.voice.channel.name}`")
+
+    @disstrack.command()
+    async def stop(self, ctx):
+        if not ctx.voice_client:
+            await ctx.send("Not connected to an voice channel")
+        elif len(ctx.me.voice.channel.members) < 2:
+            if ctx.author == ctx.guild.owner:
+                await ctx.me.voice.channel.disconnect()
+                await ctx.send("Disconnected")
+            else:
+                await ctx.send("There are members in the channel")
+        else:
+            await ctx.voice_client.disconnect()
+            await ctx.send("Disconnected")
+
+def setup(bot):
+    bot.add_cog(Music(bot))
