151f4ba57d0b23c882672a8796994aa117a6d30e
EnterNewName <40485108+enternewname@users.noreply.github.com>
2018-12-29 17:21:37 -0600

    ok

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-echo "Updating Dependencies"
-apt-get update
-apt-get upgrade
-echo "Restarting services"
-systemctl restart postgresql
 pip3 install --user -U -r requirements.txt
 echo "Running build"
 python3 build.py
